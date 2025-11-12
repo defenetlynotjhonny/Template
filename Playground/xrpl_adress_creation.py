5f7ce64fcb67d61a9a07b0360cbadd057983546c
defenetlynotjhonny <krstinikola@gmail.com>
2025-11-12 23:50:39 +0100

    First payment made

--- Playground/xrpl_adress_creation.py
+++ Playground/xrpl_adress_creation.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+client = JsonRpcClient(os.environ.get("TESTNET_URL"))
 seed_1 = os.environ.get("SEED_PHRASE_1")
 seed_2 = os.environ.get("SEED_PHRASE_2")
 seed_3 = os.environ.get("SEED_PHRASE_3")
@@ -38,3 +39,18 @@ print(f"Wallet 3 Address: {wallet_3.classic_address}, Seed: {wallet_3.seed}")
 
 
 print(WALLETS)
+
+account_info_wallet_1 = client.request(AccountInfo(account=wallet_1.classic_address))
+account_info_wallet_2 = client.request(AccountInfo(account=wallet_2.classic_address))
+account_info_wallet_3 = client.request(AccountInfo(account=wallet_3.classic_address))
+
+
+print(account_info_wallet_1)
+print("-------------------------")
+print(account_info_wallet_2)
+print("-------------------------")
+print(account_info_wallet_3)
+print("-------------------------")
+print(f"Wallet 1 Address: {wallet_1}, Balance: {drops_to_xrp(account_info_wallet_1.result['account_data']['Balance'])} XRP")
+print(f"Wallet 2 Address: {wallet_2}, Balance: {drops_to_xrp(account_info_wallet_2.result['account_data']['Balance'])} XRP")
+print(f"Wallet 3 Address: {wallet_3}, Balance: {drops_to_xrp(account_info_wallet_3.result['account_data']['Balance'])} XRP")
--- Playground/xrpl_send_xrp.py
+++ Playground/xrpl_send_xrp.py
@@ -15,16 +15,31 @@ from dotenv import load_dotenv
 load_dotenv()
 client = JsonRpcClient(os.environ.get("TESTNET_URL"))
 URL = os.environ.get("TESTNET_URL")
-wallet_1 = os.environ.get("wallet_1")
-wallet_2 = os.environ.get("wallet_2")
-wallet_3 = os.environ.get("wallet_3")
+adress_1 = os.environ.get("wallet_1")
+adress_2 = os.environ.get("wallet_2")
+adress_3 = os.environ.get("wallet_3")
+
+
+# This string is a seed
+seed_1 = os.environ.get("SEED_PHRASE_1")
+seed_2 = os.environ.get("SEED_PHRASE_2")
+seed_3 = os.environ.get("SEED_PHRASE_3")
+
+# Create the wallet from the seed
+wallet_1 = Wallet.from_seed(seed_1)
+wallet_2 = Wallet.from_seed(seed_2)
+wallet_3 = Wallet.from_seed(seed_3)
+
+
+
 WALLETS = [wallet_1, wallet_2, wallet_3]
 print("Loaded Wallets from .env:")
 print("-------------------------")
 
-account_info_wallet_1 = JsonRpcClient(URL).request(AccountInfo(account=wallet_1))
-account_info_wallet_2 = JsonRpcClient(URL).request(AccountInfo(account=wallet_2))
-account_info_wallet_3 = JsonRpcClient(URL).request(AccountInfo(account=wallet_3))
+account_info_wallet_1 = client.request(AccountInfo(account=adress_1))
+account_info_wallet_2 = client.request(AccountInfo(account=adress_2))
+account_info_wallet_3 = client.request(AccountInfo(account=adress_3))
+
 
 print(account_info_wallet_1)
 print("-------------------------")
@@ -40,17 +55,16 @@ payment_amount = 1
 payment_amount = xrp_to_drops(payment_amount) 
 
 payment_tx = Payment(
-    account=wallet_1,
-    destination=wallet_2,
-    amount=payment_amount,
+    account=wallet_1.classic_address,
+    destination=wallet_2.classic_address,
+    amount=payment_amount
 )
 
 print("Prepared Payment Transaction:")
 print(payment_tx)   
 print("-------------------------")
-transaction = Transaction()
 print("Payment Transaction is valid.")
-response = submit_and_wait(payment_tx, client, wallet_1)
+response = submit_and_wait(transaction=payment_tx, client=client,wallet=wallet_1) 
 print("Payment Transaction Response:")
 print(response)
 print("-------------------------")
